5cd508dfd335c705375c6c7f2c5c7356be3b4bf7
aliza zahid <alizayzah11@gmail.com>
2023-12-13 01:54:45 +0500

    connecting database

---
+++ connectDb.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose');
+const connectToDatabase = async function () {
+    try {
+        await mongoose.connect('mongodb+srv://aliza:aliza@cluster0.gm5wuz6.mongodb.net/');
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);}
+    }
+    module.exports=connectToDatabase;
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var express = require('express');
-var app = express();
+const mongoose = require("mongoose");
+const app = express();  // Declare 'app' once
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+let connectToDatabase = require("./connectDb");
 let users = require("./src/routes/users");
-let books = require("./src/routes/books")
+let books = require("./src/routes/books");
 
 app.use("/users", users);
 app.use("/books", books);
 
-console.log('Backend is running on port 8080.')
-app.listen(8080);
+mongoose.set("strictQuery", false);
+connectToDatabase();
+
+app.get('/', (req, res) => {
+    res.send("Hello World....");
+});
+
+console.log('Backend is running on port 8020.');
+app.listen(8020);
